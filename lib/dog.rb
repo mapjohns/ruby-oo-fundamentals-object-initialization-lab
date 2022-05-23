8df6bf06fb783760ebebf983c56cf66fcb2f659f
Matthew <mattj20@gmail.com>
2022-05-22 19:26:20 -0700

    commit tree 12c3f4fb741c3dd770892d5d0c481fb92f11da42

---
+++ lib/dog.rb
@@ -0,0 +1,14 @@
+class Dog
+    def initialize(dog_name, breed = "Mutt")
+        @name = dog_name
+        @breed = breed
+    end
+
+    def name=(dog_name)
+        @name = dog_name
+    end
+
+    def name
+        @name
+    end
+end
